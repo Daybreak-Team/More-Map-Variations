af3271c56aa57d9d4254d08bad43058316771eba
Blabla <relnarrelnar2@gmail.com>
2026-02-19 19:45:00 +0100

    Bonfire plus mount event fixes

--- event/m46_62_00_00.emevd.dcx.js
+++ event/m46_62_00_00.emevd.dcx.js
@@ -12,55 +12,62 @@ $Event(0, Default, function() {
     $InitializeCommonEvent(0, 90015000, 0, 46625800, 904311001, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625800, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625800, 46625800, 1020, 940000, 11290, 904311001, 46625800);
-    $InitializeCommonEvent(0, 9005810, 46620800, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625800, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625800);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625810, 904311002, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625810, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625810, 46625810, 1020, 940000, 11290, 904311002, 46625810);
-    $InitializeCommonEvent(0, 9005810, 46620810, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625810, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625810);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625820, 904311003, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625820, 30, 940000, 0);
-    $InitializeCommonEvent(0, 90015002, 0, 0, 46625820, 4662582, 1020, 940000, 11290, 904311003, 46625820);
-    $InitializeCommonEvent(0, 9005810, 46620820, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 90015002, 0, 0, 46625820, 46625820, 1020, 940000, 11290, 904311003, 46625820);
+    $InitializeCommonEvent(0, 9005810, 46625820, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625820);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625830, 904311004, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625830, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625830, 46625830, 1020, 940000, 11290, 904311004, 46625830);
-    $InitializeCommonEvent(0, 9005810, 46620830, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625830, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625830);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625840, 904311006, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625840, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625840, 46625840, 1020, 940000, 11290, 904311006, 46625840);
-    $InitializeCommonEvent(0, 9005810, 46620840, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625840, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625840);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625850, 904311005, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625850, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625850, 46625850, 1020, 940000, 11290, 904311005, 46625850);
-    $InitializeCommonEvent(0, 9005810, 46620850, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625850, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625850);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625860, 903700302, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625860, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625860, 46625860, 1020, 940000, 11290, 903700302, 46625860);
-    $InitializeCommonEvent(0, 9005810, 46620860, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625860, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625860);
+    
     $InitializeCommonEvent(0, 90015000, 0, 46625870, 903550542, 30, 0, 0);
     $InitializeCommonEvent(0, 90015030, 0, 46625870, 30, 940000, 0);
     $InitializeCommonEvent(0, 90015002, 0, 0, 46625870, 46625870, 1020, 940000, 11290, 903550542, 46625870);
-    $InitializeCommonEvent(0, 9005810, 46620870, 46620500, 46621500, 5);
+    $InitializeCommonEvent(0, 9005810, 46625870, 46620500, 46621500, 5);
     $InitializeCommonEvent(0, 90015005, 46621500, 46622800, 46625870);
     GotoIf(S0, Signed(0) != 0);
     GotoIf(S0, 0 != 0);
     Goto(S1);
 S0:
-    $InitializeCommonEvent(0, 90015008, 0, 46620800, 940000, 46625800, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620810, 940000, 46625810, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620820, 940000, 46625820, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620830, 940000, 46625830, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620840, 940000, 46625840, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620850, 940000, 46625850, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620860, 940000, 46625860, 0, 0);
-    $InitializeCommonEvent(0, 90015008, 0, 46620870, 940000, 46625870, 0, 0);   
+    $InitializeCommonEvent(0, 90015008, 0, 46625800, 940000, 46625800, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625810, 940000, 46625810, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625820, 940000, 46625820, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625830, 940000, 46625830, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625840, 940000, 46625840, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625850, 940000, 46625850, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625860, 940000, 46625860, 0, 0);
+    $InitializeCommonEvent(0, 90015008, 0, 46625870, 940000, 46625870, 0, 0);   
 S1:
     if (0 != 0) {
         $InitializeCommonEvent(0, 90015013);
@@ -99,29 +106,27 @@ S1:
         $InitializeCommonEvent(0, 90015071, 46625870);
         $InitializeCommonEvent(0, 90015468, 46625870, 8270, 8247);
     }
-    $InitializeCommonEvent(0, 90065111, 46625800, 46625810, 6001, 46625800);
-    $InitializeCommonEvent(0, 90065112, 46625800, 46625810, 6001, 46625800);
+    // Riding mount events
+    $InitializeCommonEvent(0, 90065111, 46620800, 46625802, 6001, 46620800);
+    $InitializeCommonEvent(0, 90065112, 46620800, 46625802, 6001, 46620800);
     $InitializeCommonEvent(0, 90015199, 46625800);
-    $InitializeCommonEvent(0, 90065111, 46625810, 46625830, 6001, 46625810);
-    $InitializeCommonEvent(0, 90065112, 46625810, 46625830, 6001, 46625810);
+    $InitializeCommonEvent(0, 90065111, 46620810, 46625812, 6001, 46620810);
+    $InitializeCommonEvent(0, 90065112, 46620810, 46625812, 6001, 46620810);
     $InitializeCommonEvent(0, 90015199, 46625810);
-    $InitializeCommonEvent(0, 90065111, 46625820, 46625850, 6001, 46625820);
-    $InitializeCommonEvent(0, 90065112, 46625820, 46625850, 6001, 46625820);
+    $InitializeCommonEvent(0, 90065111, 46620820, 46625822, 6001, 46620820);
+    $InitializeCommonEvent(0, 90065112, 46620820, 46625822, 6001, 46620820);
     $InitializeCommonEvent(0, 90015199, 46625820);
-    $InitializeCommonEvent(0, 90065111, 46625830, 46620870, 6001, 46625830);
-    $InitializeCommonEvent(0, 90065112, 46625830, 46620870, 6001, 46625830);
+    $InitializeCommonEvent(0, 90065111, 46620830, 46620832, 6001, 46620830);
+    $InitializeCommonEvent(0, 90065112, 46620830, 46620832, 6001, 46620830);
     $InitializeCommonEvent(0, 90015199, 46625830);
-    $InitializeCommonEvent(0, 90065111, 46625840, 46620890, 6001, 46625840);
-    $InitializeCommonEvent(0, 90065112, 46625840, 46620890, 6001, 46625840);
+    $InitializeCommonEvent(0, 90065111, 46620840, 46620842, 6001, 46620840);
+    $InitializeCommonEvent(0, 90065112, 46620840, 46620842, 6001, 46620840);
     $InitializeCommonEvent(0, 90015199, 46625840);
-    $InitializeCommonEvent(0, 90065111, 46625850, 46620910, 6001, 46625850);
-    $InitializeCommonEvent(0, 90065112, 46625850, 46620910, 6001, 46625850);
+    $InitializeCommonEvent(0, 90065111, 46620850, 46620852, 6001, 46625050);
+    $InitializeCommonEvent(0, 90065112, 46620850, 46620852, 6001, 46625050);
     $InitializeCommonEvent(0, 90015199, 46625850);
-    $InitializeCommonEvent(0, 90065111, 46625860, 46620910, 6001, 46625860);
-    $InitializeCommonEvent(0, 90065112, 46625860, 46620910, 6001, 46625860);
+    
     $InitializeCommonEvent(0, 90015199, 46625860);
-    $InitializeCommonEvent(0, 90065111, 46625870, 46620910, 6001, 46625870);
-    $InitializeCommonEvent(0, 90065112, 46625870, 46620910, 6001, 46625870);
     $InitializeCommonEvent(0, 90015199, 46625870);  
     if (IsPlayMode(2)) {
         $InitializeCommonEvent(0, 90015040, 0);
