7fd92a725237a2672ea0ff8854f46b350afb40b8
KevinS <hanwenshi6@gmail.com>
2026-04-19 19:04:31 -0700

    temples

--- event/m40_10_00_00.emevd.dcx.js
+++ event/m40_10_00_00.emevd.dcx.js
@@ -8,100 +8,67 @@
 // ==/EMEVD==
 
 $Event(0, Default, function() {
-    $InitializeEvent(0, 40102500);
-    $InitializeEvent(0, 40102503);
-    $InitializeEvent(0, 40102505);
-    $InitializeEvent(1, 40102580, 1, 40101601, 40101621, 40100581, 807390);
-    $InitializeEvent(1, 40102590, 1, 40101601, 40100591);
-    $InitializeEvent(1, 40102580, 2, 40101601, 40101621, 40100581, 807390);
-    $InitializeEvent(1, 40102590, 2, 40101601, 40100591);
-    $InitializeEvent(1, 40102580, 3, 40101601, 40101621, 40100581, 807390);
-    $InitializeEvent(1, 40102590, 3, 40101601, 40100591);    
+    if (IsMapVariation(1)) {
+        $InitializeEvent(1, 40102500, 1, 40100500, 40101597, 40101587);
+        $InitializeEvent(1, 40102503, 1, 40100500);
+        $InitializeEvent(1, 40102505, 1, 40100500);
+        $InitializeEvent(1, 40102590, 1, 40100510, 40100500, 40105000);
+    } else if (IsMapVariation(2)) {
+        $InitializeEvent(2, 40102500, 2, 40100501, 40101597, 40101587);
+        $InitializeEvent(2, 40102503, 2, 40100501);
+        $InitializeEvent(2, 40102505, 2, 40100501);
+        $InitializeEvent(2, 40102590, 2, 40100511, 40100501, 40105010);
+    } else if (IsMapVariation(3)) {
+        $InitializeEvent(3, 40102500, 3, 40100502, 40101597, 40101587);
+        $InitializeEvent(3, 40102503, 3, 40100502);
+        $InitializeEvent(3, 40102505, 3, 40100502);
+        $InitializeEvent(3, 40102590, 3, 40100512, 40100502, 40105020);
+    }
     $InitializeCommonEvent(0, 90015500, 40102320);
 });
 
-$Event(40102500, Restart, function() {
-    if (EventFlag(40100500)) {
-        DisableAsset(40101597);
-        DisableAsset(40101598);
+$Event(40102500, Restart, function(mapVariationId, eventFlagId, assetEntityId, assetEntityId2) {
+    EndIf(!IsMapVariation(mapVariationId));
+    if (EventFlag(eventFlagId)) {
+        DisableAsset(assetEntityId);
+        DisableAsset(assetEntityId2);
         EndEvent();
     }
 L0:
-    //CreateAssetfollowingSFX(40101597, 101, 800748);
-    //CreateAssetfollowingSFX(40101598, 101, 800746);
-    WaitFor(EventFlag(40100500));
-    DeleteAssetfollowingSFX(40101597, true);
-    DisableAsset(40101597);
-    DeleteAssetfollowingSFX(40101598, true);
-    DisableAsset(40101598);
-    DeleteMapSFX(40101598, 0);
-    PlaySE(40101597, SoundType.SFX, 1500);
+    WaitFor(EventFlag(eventFlagId));
+    DeleteAssetfollowingSFX(assetEntityId, true);
+    DisableAsset(assetEntityId);
+    DeleteAssetfollowingSFX(assetEntityId2, true);
+    DisableAsset(assetEntityId2);
+    DeleteMapSFX(assetEntityId2, false);
+    PlaySE(assetEntityId, SoundType.SFX, 1500);
     WaitFixedTimeSeconds(0.5);
-    RecordUserDispLog(11225, 40101599, LogObjectType.None, -1);
+    RecordUserDispLog(11225, assetEntityId, LogObjectType.None, -1);
 });
 
-
-// Changes icon after unlock
-$Event(40102503, Default, function() {
-    WaitFor(EventFlag(40100500));
+$Event(40102503, Default, function(mapVariationId, eventFlagId) {
+    EndIf(!IsMapVariation(mapVariationId));
+    WaitFor(EventFlag(eventFlagId));
     SetWorldMapPointFlag(6, ON);
     SetWorldMapPointFlag(8, OFF);
 });
 
-$Event(40102504, Default, function() {
-    DisableNetworkSync();
-});
-
-// For the rise mana relic
-$Event(40102505, Default, function() {
-    EndIf(EventFlag(40100500));
-    WaitFor(EventFlag(40100500));
+$Event(40102505, Default, function(mapVariationId, eventFlagId) {
+    EndIf(!IsMapVariation(mapVariationId));
+    EndIf(EventFlag(eventFlagId));
+    WaitFor(EventFlag(eventFlagId));
     IncrementNetworkconnectedEventValue(8140, 4, 7);
 });
 
-
-$Event(40102580, Restart, function(mapVariationId, assetEntityId, assetEntityId2, eventFlagId, sfxId) {
+$Event(40102590, Restart, function(mapVariationId, eventFlagId, eventFlagId2, chrEntityId) {
     EndIf(!IsMapVariation(mapVariationId));
-    if (EventFlag(40100501)) {
-        DisableAsset(assetEntityId2);
-        if (!EventFlag(40100502)) {
-            EnableAsset(assetEntityId);
-        }
-        if (EventFlag(40100502)) {
-            DisableAsset(assetEntityId);
-        }
+    if (EventFlag(eventFlagId)) {
         EndEvent();
     }
 L0:
-    DisableAsset(assetEntityId);
-    WaitFor(AssetHP(assetEntityId2) != 9999);
-    SetSpEffect(assetEntityId2, 0);
+    WaitFor(CharacterRatioDead(chrEntityId));
     SetNetworkconnectedEventFlagID(eventFlagId, ON);
-    SetNetworkconnectedEventFlagID(40100501, ON);
-    WaitFixedTimeSeconds(0.1);
-    
-    ForceAnimationPlayback(assetEntityId2, 1, false, false, false);
-    SpawnOneshotSFX(TargetEntityType.Asset, assetEntityId2, 100, sfxId);
-    WaitFixedTimeSeconds(2);
-
-    DisableAsset(assetEntityId2);
-    WaitFixedTimeSeconds(0.1);
-    EnableAsset(assetEntityId);
-    ForceAnimationPlayback(assetEntityId, 1, false, true, false);
+    SetNetworkconnectedEventFlagID(eventFlagId2, ON);
 });
 
-// Sets event flag for specific variation. Used to only affect one rise. 
-// Also means all rise will need diff variations. Or just use one per map.
-$Event(40102590, Restart, function(mapVariationId, assetEntityId, eventFlagId) {
-    EndIf(!IsMapVariation(mapVariationId));
-    if (EventFlag(40100502)) {
-        DisableAsset(assetEntityId);
-        EndEvent();
-    }
-L0:
-    WaitFor(CharacterRatioDead(40105000));
-    SetSpEffect(10002, 1605000);
-    SetNetworkconnectedEventFlagID(eventFlagId, ON);
-    SetNetworkconnectedEventFlagID(40100502, ON);
-    SetNetworkconnectedEventFlagID(40100500, ON);
-});
+
